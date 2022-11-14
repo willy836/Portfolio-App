e149d4d114a6c1dbdd7df41b031fb4ac3bf84510
willy836 <wilsonkimiri836@gmail.com>
2022-11-14 12:20:31 +0300

    Implement mobile menu functionality with js

---
+++ main.js
@@ -0,0 +1,16 @@
+// Mobile menu
+const hamburger = document.querySelector('.hamburger');
+const links = document.querySelector('.links');
+const link = document.querySelectorAll('.link');
+
+hamburger.addEventListener('click', ()=> {
+    hamburger.classList.toggle('active');
+    links.classList.toggle('show-links');
+});
+
+link.forEach((linkItem)=> {
+    linkItem.addEventListener('click', ()=> {
+        hamburger.classList.remove('active');
+        links.classList.remove('show-links');
+    })
+})
