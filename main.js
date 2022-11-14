b86a4536cf1a0f0f152ddd99dc42821ba598ded0
willy836 <wilsonkimiri836@gmail.com>
2022-11-14 14:25:48 +0300

    Fix eslint errors

--- main.js
+++ main.js
@@ -3,14 +3,14 @@ const hamburger = document.querySelector('.hamburger');
 const links = document.querySelector('.links');
 const link = document.querySelectorAll('.link');
 
-hamburger.addEventListener('click', ()=> {
-    hamburger.classList.toggle('active');
-    links.classList.toggle('show-links');
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('active');
+  links.classList.toggle('show-links');
 });
 
-link.forEach((linkItem)=> {
-    linkItem.addEventListener('click', ()=> {
-        hamburger.classList.remove('active');
-        links.classList.remove('show-links');
-    })
-})
+link.forEach((linkItem) => {
+  linkItem.addEventListener('click', () => {
+    hamburger.classList.remove('active');
+    links.classList.remove('show-links');
+  });
+});
